6e0a458e862574dc43473fef7a5aa80aca0ef6f3
Tamir Bahar <tamir.bahar@gmail.com>
2024-07-28 18:13:09 +0300

    Removed prints

--- wakelock.js
+++ wakelock.js
@@ -1,12 +1,9 @@
 // test support
 let isSupported = false;
 
-
 if ('wakeLock' in navigator) {
     isSupported = true;
 }
-
-window.alert("Wo!")
 
 if (isSupported) {
     // create a reference for the wake lock
@@ -16,10 +13,8 @@ if (isSupported) {
     const requestWakeLock = async () => {
         try {
             wakeLock = await navigator.wakeLock.request('screen');
-            window.alert("Success!")
         } catch (err) {
             // if wake lock request fails - usually system related, such as battery
-            log("How'd I get here?");
             console.log(err);
         }
     } // requestWakeLock()
@@ -35,6 +30,3 @@ if (isSupported) {
     // Losing visibility releases the lock, so we make sure to request it again.
     document.addEventListener('visibilitychange', handleVisibilityChange);
 } // isSupported
-else {
-    window.alert("Not supported!");
-}
